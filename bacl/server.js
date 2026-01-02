8df0efab0aa0386e5f58bb5e1596da8a33316ea7
Alwayshari1 <gupteswarraohari0000@gmail.com>
2026-01-02 18:44:43 +0530

    back fixed

--- bacl/server.js
+++ bacl/server.js
@@ -44,8 +44,8 @@ const app = express();
 
 // Allowed origins for CORS
 const allowedOrigins = [
-  'http://localhost:3000',
-  'http://localhost:5173',
+  // 'http://localhost:3000',
+  // 'http://localhost:5173',
   'https://rest-frontend-three.vercel.app'
 ];
 
